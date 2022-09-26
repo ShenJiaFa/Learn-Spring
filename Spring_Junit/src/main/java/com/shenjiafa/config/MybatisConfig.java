9637470b068d50e2063c85c4587a8a91051018af
shenjiafa <sjfdzh@gmail.com>
2022-09-26 16:01:20 +0800

    [Description]:修复部分细节

--- Spring_Junit/src/main/java/com/shenjiafa/config/MybatisConfig.java
+++ Spring_Junit/src/main/java/com/shenjiafa/config/MybatisConfig.java
@@ -1,6 +1,5 @@
 package com.shenjiafa.config;
 
-import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.mapper.MapperScannerConfigurer;
 import org.springframework.context.annotation.Bean;
